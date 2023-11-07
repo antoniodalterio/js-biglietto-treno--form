98e801239cb825d81a16cc2b1f39e789c65efd8b
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-07 21:58:20 +0100

    feat: milestone 2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@
 /* MILESTONE 1:
 Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console. */
 
-const bottoneCalc = document.querySelector('input[type="submit"]');
+const bottoneCalc = document.querySelector('button');
 
 console.log(bottoneCalc);
 bottoneCalc.addEventListener('click', function () {
@@ -13,7 +13,7 @@ bottoneCalc.addEventListener('click', function () {
   const kilometriViaggio = Number(document.getElementById('kilometri').value);
   console.log(kilometriViaggio);
 
-  const prezzoBiglietto = Number(kilometriViaggio * 0.21).toFixed(2);
+  let prezzoBiglietto = Number(kilometriViaggio * 0.21).toFixed(2);
   console.log(prezzoBiglietto);
 
   const passeggeroEta = document.getElementById('eta').value;
@@ -27,11 +27,25 @@ bottoneCalc.addEventListener('click', function () {
     scontoBigliettoMinori = Number(
       prezzoBiglietto - (prezzoBiglietto / 100) * 20
     ).toFixed(2);
-    console.log(scontoBigliettoMinori);
+    prezzoBiglietto = scontoBigliettoMinori;
+    console.log(prezzoBiglietto);
   } else if (passeggeroEta === over65) {
     scontoBigliettoOver65 = Number(
       prezzoBiglietto - (prezzoBiglietto / 100) * 40
     ).toFixed(2);
-    console.log(scontoBigliettoOver65);
+    prezzoBiglietto = scontoBigliettoOver65;
+    console.log(prezzoBiglietto);
   }
+
+  const nomeSuBiglietto = document.getElementById('nome-passeggero');
+  nomeSuBiglietto.innerHTML = personName;
+
+  const carrozzaNumero = document.getElementById('carrozza');
+  carrozzaNumero.innerHTML = Math.round(Math.random() * 5);
+
+  const codiceCp = document.getElementById('codice');
+  codiceCp.innerHTML = Math.round(Math.random() * 10000);
+
+  const costoBiglietto = document.getElementById('costo-biglietto');
+  costoBiglietto.innerHTML = prezzoBiglietto + '€';
 });
