2d2412e4f6793deda30a26341b99e18aaae829a8
antoniodalterio <antoniodalterio2227@gmail.com>
2023-11-08 14:13:17 +0100

    fix: aggiunti commenti

--- js/main.js
+++ js/main.js
@@ -7,50 +7,39 @@ const bottoneCalc = document.querySelector('#btn-genera');
 const bottoneReset = document.querySelector('#btn-annulla');
 const comparsaCalcolo = document.getElementById('sezione-calcolo');
 
-console.log(bottoneCalc);
+// Campi biglietto virtuale
+const nomeSuBiglietto = document.getElementById('nome-passeggero');
+const carrozzaNumero = document.getElementById('carrozza');
+const codiceCp = document.getElementById('codice');
+const costoBiglietto = document.getElementById('costo-biglietto');
+
+// Funzione bottone Genera
+
 bottoneCalc.addEventListener('click', function () {
   const personName = document.getElementById('nome').value;
-  console.log(personName);
 
   const kilometriViaggio = Number(document.getElementById('kilometri').value);
-  console.log(kilometriViaggio);
 
   let prezzoBiglietto = Number(kilometriViaggio * 0.21).toFixed(2);
-  console.log(prezzoBiglietto);
 
   const passeggeroEta = document.getElementById('eta').value;
-  console.log(passeggeroEta);
-  const minorenne = document.getElementById('minore').value;
-  console.log(minorenne);
-  const over65 = document.getElementById('over65').value;
-  let scontoBigliettoMinori = 0;
-  let scontoBigliettoOver65 = 0;
-  if (passeggeroEta == minorenne) {
-    scontoBigliettoMinori = Number(
+
+  if (passeggeroEta === 'minorenne') {
+    prezzoBiglietto = Number(
       prezzoBiglietto - (prezzoBiglietto / 100) * 20
     ).toFixed(2);
-    prezzoBiglietto = scontoBigliettoMinori;
-    console.log(prezzoBiglietto);
-  } else if (passeggeroEta === over65) {
-    scontoBigliettoOver65 = Number(
+  } else if (passeggeroEta === 'over65') {
+    prezzoBiglietto = Number(
       prezzoBiglietto - (prezzoBiglietto / 100) * 40
     ).toFixed(2);
-    prezzoBiglietto = scontoBigliettoOver65;
-    console.log(prezzoBiglietto);
   }
-
-  const nomeSuBiglietto = document.getElementById('nome-passeggero');
+  //  valori generati nel biglietto
   nomeSuBiglietto.innerHTML = personName;
-
-  const carrozzaNumero = document.getElementById('carrozza');
   carrozzaNumero.innerHTML = Math.round(Math.random() * 5);
-
-  const codiceCp = document.getElementById('codice');
   codiceCp.innerHTML = Math.round(Math.random() * 10000);
-
-  const costoBiglietto = document.getElementById('costo-biglietto');
   costoBiglietto.innerHTML = prezzoBiglietto + '€';
 
+  // comparsa biglietto
   comparsaCalcolo.classList.add('d-block');
   comparsaCalcolo.classList.remove('d-none');
 });
